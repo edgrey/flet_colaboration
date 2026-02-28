2fbb251ed6c1ab71e6c2dd330685858abbb63612
Zelkra <lush.matdip@gmail.com>
2026-02-28 00:47:19 -0600

    PRIMER CAMBIO (CONFIGURACION DE LA VENTANA)

--- flet_1.py
+++ flet_1.py
@@ -2,6 +2,24 @@
 # PROYECTO RED DE CODIGO
 ###
 
-print('Aqui se colocara el proyecto pensado')
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = 'Calculator'
+    page.window.width = 320
+    page.window.height = 450
+    page.bgcolor = ft.Colors.GREY_900
+    page.window.resizable = False
+    page.window.maximizable = False
+    
+
+    
+    page.add(ft.Row([
+        ft.ElevatedButton('1', bgcolor=ft.Colors.TRANSPARENT, color=ft.Colors.WHITE)
+    ]))
+    
+    
+   
 
 
+ft.app(target=main)
